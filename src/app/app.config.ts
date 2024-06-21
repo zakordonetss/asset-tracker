10d9c5995eb66a3d78969ef56c474f1449f2268c
Dmytro Zakordonets <zakordonets@Dmytros-MacBook-Air.local>
2024-06-21 10:16:26 +0200

    AT: added styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
   ],
 };
--- src/app/components/assets/assets.component.ts
+++ src/app/components/assets/assets.component.ts
@@ -8,30 +8,32 @@ import {
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { Subscription, Observable } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  startWith,
+  catchError,
+  map,
+} from 'rxjs/operators';
 import * as Highcharts from 'highcharts/highstock';
 import HighchartsMore from 'highcharts/highcharts-more';
 import HighchartsExporting from 'highcharts/modules/exporting';
 import HighchartsAccessibility from 'highcharts/modules/accessibility';
 import { WebSocketService } from 'src/app/services/websocket.service';
-import {
-  Observable,
-  Subscription,
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  startWith,
-  switchMap,
-} from 'rxjs';
 import { PriceApiService } from 'src/app/services/price-api.service';
 import { CommonModule } from '@angular/common';
+import { InstrumentsApiService } from 'src/app/services/instruments-api.service';
 import {
   EProvider,
+  IMarketOrder,
   IPriceData,
   IPriceQueryParams,
   IWebSocketMessage,
 } from '@models';
-import { InstrumentsApiService } from 'src/app/services/instruments-api.service';
 
 HighchartsMore(Highcharts);
 HighchartsExporting(Highcharts);
@@ -47,20 +49,22 @@ HighchartsAccessibility(Highcharts);
     MatAutocompleteModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
   ],
   standalone: true,
 })
 export class AssetsComponent implements OnInit, OnDestroy {
-  @ViewChild('priceChart', { static: true }) priceChart: ElementRef;
+  @ViewChild('priceChart', { static: true }) private _priceChart: ElementRef;
 
-  public l1Data$: Observable<IWebSocketMessage> =
-    this._webSocketService.messages$;
-  public symbolControl = new FormControl();
-  public filteredSymbols$: Observable<string[]>;
+  symbolControl = new FormControl('USD/PLN');
+  filteredSymbols$: Observable<string[]>;
+  marketOrder: IMarketOrder;
+
   private _websocketSubscription: Subscription;
   private _priceHistorySubscription: Subscription;
   private _chart: Highcharts.Chart;
-  private instrumentId: string = 'ad9e5345-4c3b-41fc-9437-1d253f62db52';
+  private _instrumentId: string;
+  private readonly _defaultKindInstrument = 'forex';
 
   constructor(
     private readonly _webSocketService: WebSocketService,
@@ -70,8 +74,24 @@ export class AssetsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.setupSymbolAutocomplete();
-    this.sendInitialWebSocketMessage();
-    this.subscribeToL1Data();
+    this.loadInitialPriceHistory();
+    this.subscribeToSymbolChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll();
+  }
+
+  private unsubscribeAll(): void {
+    if (this._websocketSubscription) {
+      this._websocketSubscription.unsubscribe();
+    }
+    if (this._priceHistorySubscription) {
+      this._priceHistorySubscription.unsubscribe();
+    }
+    if (this._chart) {
+      this._chart.destroy();
+    }
   }
 
   private setupSymbolAutocomplete(): void {
@@ -79,13 +99,13 @@ export class AssetsComponent implements OnInit, OnDestroy {
       startWith(''),
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((value) => this.filterSymbols(value))
+      switchMap((value) => this.filterSymbols(value ?? ''))
     );
   }
 
   private filterSymbols(value: string): Observable<string[]> {
     return this._instrumentsService
-      .getSymbols(EProvider.Oanda, 'forex')
+      .getSymbols(EProvider.Oanda, this._defaultKindInstrument)
       .pipe(
         map((symbols) =>
           symbols.filter((symbol) =>
@@ -95,74 +115,118 @@ export class AssetsComponent implements OnInit, OnDestroy {
       );
   }
 
-  private sendInitialWebSocketMessage(): void {
-    const subscriptionMessage = {
-      type: 'l1-subscription',
-      id: '1',
-      instrumentId: this.instrumentId,
-      provider: 'simulation',
-      subscribe: true,
-      kinds: ['ask', 'bid', 'last'],
-    };
-    this._webSocketService.connect();
-    this._websocketSubscription = this._webSocketService.messages$.subscribe();
-    this._webSocketService.sendMessage(subscriptionMessage);
-    this.loadPriceHistory(); // Load initial price history
-  }
-
-  private subscribeToL1Data(): void {
+  private subscribeToSymbolChanges(): void {
     this.symbolControl.valueChanges
-      .pipe(debounceTime(300), distinctUntilChanged())
-      .subscribe((symbol) => {
-        if (symbol) {
-          this._instrumentsService
-            .getInstrumentIdBySymbol(symbol, EProvider.Oanda, 'forex')
-            .subscribe((instrumentId) => {
-              if (instrumentId) {
-                this.instrumentId = instrumentId;
-                const subscriptionMessage = {
-                  type: 'l1-subscription',
-                  id: '1',
-                  instrumentId: this.instrumentId,
-                  provider: 'simulation',
-                  subscribe: true,
-                  kinds: ['ask', 'bid', 'last'],
-                };
-                this._webSocketService.sendMessage(subscriptionMessage);
-                this.loadPriceHistory(); // Update price history on symbol change
-              } else {
-                console.error(`Instrument ID not found for symbol: ${symbol}`);
-              }
-            });
-        }
-      });
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((symbol) =>
+          this.fetchInstrumentIdAndLoadPriceHistory(symbol ?? '')
+        )
+      )
+      .subscribe();
   }
 
-  private loadPriceHistory(): void {
-    if (!this.instrumentId) return; // Ensure we have an instrumentId before proceeding
+  private fetchInstrumentIdAndLoadPriceHistory(
+    symbol: string
+  ): Observable<void> {
+    return this._instrumentsService
+      .getInstrumentIdBySymbol(
+        symbol,
+        EProvider.Oanda,
+        this._defaultKindInstrument
+      )
+      .pipe(
+        switchMap((instrumentId) => {
+          if (instrumentId) {
+            this._instrumentId = instrumentId;
+            return this.loadPriceHistory();
+          } else {
+            console.error(`Instrument ID not found for symbol: ${symbol}`);
+            return [];
+          }
+        })
+      );
+  }
+
+  loadInitialPriceHistory(): void {
+    this.fetchInstrumentIdAndLoadPriceHistory('USD/PLN').subscribe();
+  }
+
+  loadPriceHistory(): Observable<void> {
+    if (!this._instrumentId) {
+      return new Observable();
+    }
     const queryParams: IPriceQueryParams = {
       provider: 'oanda',
       interval: 1,
       periodicity: 'day',
       startDate: '2023-06-08',
       endDate: '2024-06-08',
-      instrumentId: this.instrumentId,
+      instrumentId: this._instrumentId,
     };
     if (this._priceHistorySubscription) {
       this._priceHistorySubscription.unsubscribe();
     }
-    this._priceHistorySubscription = this._priceApiService
-      .getPriceData(queryParams)
-      .subscribe({
-        next: (response) => this.renderChart(response.data),
-        error: (error) => console.error('Error fetching price history', error),
+    return this._priceApiService.getPriceData(queryParams).pipe(
+      map((response) => {
+        this.renderChart(response.data);
+      }),
+      catchError((error) => {
+        console.error('Error fetching price history', error);
+        return [];
+      })
+    );
+  }
+
+  subscribeToWebSocket(): void {
+    const symbol = this.symbolControl.value;
+    if (symbol) {
+      this.fetchInstrumentIdAndSubscribeToWebSocket(symbol);
+    }
+  }
+
+  private fetchInstrumentIdAndSubscribeToWebSocket(symbol: string): void {
+    this._instrumentsService
+      .getInstrumentIdBySymbol(
+        symbol,
+        EProvider.Oanda,
+        this._defaultKindInstrument
+      )
+      .subscribe((instrumentId) => {
+        if (instrumentId) {
+          this._instrumentId = instrumentId;
+          const subscriptionMessage = {
+            type: 'l1-subscription',
+            id: '1',
+            instrumentId: this._instrumentId,
+            provider: 'simulation',
+            subscribe: true,
+            kinds: ['ask', 'bid', 'last'],
+          };
+          this._webSocketService.connect();
+          if (this._websocketSubscription) {
+            this._websocketSubscription.unsubscribe();
+          }
+          this._websocketSubscription =
+            this._webSocketService.messages$.subscribe(
+              (message: IWebSocketMessage) => {
+                if (message.last) {
+                  this.marketOrder = message.last;
+                }
+              }
+            );
+          this._webSocketService.sendMessage(subscriptionMessage);
+        } else {
+          console.error(`Instrument ID not found for symbol: ${symbol}`);
+        }
       });
   }
 
   private renderChart(priceData: IPriceData[]): void {
     const chartOptions: Highcharts.Options = {
       title: {
-        text: 'Price Data',
+        text: `Price History for ${this.symbolControl.value}`,
       },
       xAxis: {
         type: 'datetime',
@@ -222,20 +286,8 @@ export class AssetsComponent implements OnInit, OnDestroy {
     }
 
     this._chart = Highcharts.stockChart(
-      this.priceChart.nativeElement,
+      this._priceChart.nativeElement,
       chartOptions
     );
-  }
-
-  ngOnDestroy(): void {
-    if (this._websocketSubscription) {
-      this._websocketSubscription.unsubscribe();
-    }
-    if (this._priceHistorySubscription) {
-      this._priceHistorySubscription.unsubscribe();
-    }
-    if (this._chart) {
-      this._chart.destroy();
-    }
   }
 }
